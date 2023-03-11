337e335a45ef22ac47b29a42fc27bcd35f5ff780
Edward <eavillamila@unal.edu.co>
2023-03-10 21:20:37 -0500

    0 Prueba Shell

--- pushGit.sh
+++ pushGit.sh
@@ -6,14 +6,14 @@ echo -e '\nPresiona ENTER para continuar...'
 read -p ''
 
 clear
-echo -e 'Se agregan los archivos al entorno de ttrabajo...\n'
+echo -e 'Se agregan los archivos al entorno de prueba...\n'
 git add .
 echo -e '\nPresiona ENTER para continuar...'
 read -p ''
 
 clear
 echo -e 'Se realiza el commit del repositorio...\n'
-read -p 'Ingresa el comentario (Sugerencia: En la forma "#Comment")' comment
+read -p 'Ingresa el comentario (Sugerencia - En la forma "#Comment"): ' comment
 git commit -m "$comment"
 echo -e '\nPresiona ENTER para continuar...'
 read -p ''
@@ -22,6 +22,10 @@ clear
 echo -e 'Se realiza el push hacia el repositorio remoto...\n'
 read -p 'Ingresa el nombre de la rama: ' rama
 git push origin "$rama"
+echo -e '\nPresiona ENTER para continuar...'
+read -p ''
+
+git status
 echo -e '\nPresiona ENTER para continuar...'
 read -p ''
 
